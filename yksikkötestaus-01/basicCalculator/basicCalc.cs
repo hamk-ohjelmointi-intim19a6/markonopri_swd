7e734f29aa72270c2b8c85010fd355ec87487581
Marko Nopri <marko.nopri@student.hamk.fi>
2020-02-04 13:23:30 +0200

    poistettu turha basic_calc ja lisätty yksikkötestaus-01

---
+++ yksikkötestaus-01/basicCalculator/basicCalc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicCalculator
+{
+    class basicCalc
+    {
+        public int Add(int x, int y)    // funktio yhteenlaskulle
+        {
+            return x + y;
+        }
+        public int Sub(int x, int y)    // funktio vähennyslaskulle
+        {
+            return x - y;
+        }
+        public int Mul(int x, int y)    // funktio kertolaskulle
+        {
+            return x * y;
+        }
+        public int Div(int x, int y)    // funktio jakolaskulle
+        {
+            return x / y;
+        }
+        public int Jak(int x, int y)    // funktio jakojäännökselle
+        {
+            return x % y;
+        }
+    }
+}
---
+++ yksikkötestaus-01/basicCalculator/basicCalcCase.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicCalculator
+{
+    [TestFixture]
+    class basicCalcCase
+    {
+        [TestCase]
+
+    public void Add()
+        {
+            basicCalc math = new basicCalc();
+            Assert.AreEqual(25, math.Add(20, 5));
+        }
+
+        [TestCase]
+    public void Sub()
+        {
+            basicCalc math = new basicCalc();
+            Assert.AreEqual(25, math.Sub(30, 5));
+        }
+
+        [TestCase]
+    public void Mul()
+        {
+            basicCalc math = new basicCalc();
+            Assert.AreEqual(25, math.Mul(5, 5));
+        }
+
+        [TestCase]
+    public void Div()
+        {
+            basicCalc math = new basicCalc();
+            Assert.AreEqual(25, math.Div(100, 4));
+        }
+
+        [TestCase]
+    public void Jak()
+        {
+            basicCalc math = new basicCalc();
+            Assert.AreEqual(0, math.Jak(5, 5));
+        }
+    }
+}
